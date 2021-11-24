4768b7f6939d8e5d0d30b6c14329902cc445c6ff
AseerUzZaman <aseer@updot.in>
2021-11-24 15:28:40 +0530

    change svgs to pngs and fix the members styles

--- src/components/aboutUs/team/member/Member.jsx
+++ src/components/aboutUs/team/member/Member.jsx
@@ -1,54 +1,37 @@
 import { useState } from "react";
 import classes from "./Member.module.css";
 
-// const image = require.context("../../../../assets/img/teams/");
-const Member = (props) => {
-  const [bnwImg, setBnwImg] = useState("");
-  const [colorImg, setColorImg] = useState("");
-  // const iconImage = image(`./${props.img}-bnw.svg`).default;
-  // const iconImageColor = image(`./${props.img}.svg`);
+import linkedinIcon from "../../../../assets/img/linkedin-icon-color.svg";
+
+const Member = ({ img, data }) => {
+  const [image, setImage] = useState("");
+
   (function (imgName) {
-    import(`../../../../assets/img/teams/${imgName}-bnw.svg`).then((image) =>
-      setBnwImg(image.default)
+    import(`../../../../assets/img/teams/${imgName}.png`).then((obj) =>
+      setImage(obj.default)
     );
-  })(props.img);
-  (function (imgName) {
-    import(`../../../../assets/img/teams/${imgName}.svg`).then((image) =>
-      setColorImg(image.default)
-    );
-  })(props.img);
+  })(img);
+
   return (
-    <div className={classes["member"]}>
-      <div className={classes["member-img-container"]}>
-        <span className={classes["member-img-bg"]}></span>
-        {bnwImg && (
-          <img
-            src={bnwImg}
-            alt={props.data.name}
-            className={classes["bnw-img"]}
-          />
-        )}
-        {colorImg && (
-          <img
-            src={colorImg}
-            alt={props.data.name}
-            className={classes["color-img"]}
-          />
-        )}
+    <>
+      <div className={classes["member"]}>
+        <div className={classes["member-img-container"]}>
+          <span className={classes["member-img-bg"]}></span>
+          <div className={classes["img-wrapper"]}>
+            <img src={image} alt={data.name} className={classes["color-img"]} />
+          </div>
+        </div>
+        <div className={classes["member-text"]}>
+          <h4 className={classes["name"]}>
+            <a href="/" target="_blank">
+              <img src={linkedinIcon} alt={`${data.name}'s Linkedin`} />
+              <span>{data.name}</span>
+            </a>
+          </h4>
+          <p className={classes["position"]}>{data.position}</p>
+        </div>
       </div>
-      <div className={classes["member-text"]}>
-        <h4 className={classes["name"]}>
-          <a href="/" target="_blank">
-            <img
-              src={props.linkedinIcon}
-              alt={`${props.data.name}'s Linkedin`}
-            />
-            <span>{props.data.name}</span>
-          </a>
-        </h4>
-        <p className={classes["position"]}>{props.data.position}</p>
-      </div>
-    </div>
+    </>
   );
 };
 
