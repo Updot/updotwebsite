3aad78128976540f604c35396aefc373b1bf056f
Prajjwal Arora <aroraprajjwal@gmail.com>
2021-12-28 22:38:30 +0530

    modal issue fix

--- src/components/ui/modal/Modal.jsx
+++ src/components/ui/modal/Modal.jsx
@@ -1,6 +1,20 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import ReactDOM from "react-dom";
 import classes from "./Modal.module.scss";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("portal");
 
 export default function Modal({ isModalOpen, setIsModalOpen, children }) {
   const clickHandle = (e) => {
@@ -22,17 +36,16 @@ export default function Modal({ isModalOpen, setIsModalOpen, children }) {
   return ReactDOM.createPortal(
     <>
       {isModalOpen && (
-        <div
-          onClick={clickHandle}
-          key={Math.random()}
-          className={`modal-container`}
-        >
-          <div className={classes["overlay"]}></div>
-          <div className={classes["modal"]}>
-            <div className={classes["close"]}>X</div>
-            {children}
-          </div>
-        </div>
+        <Fragment>
+          {ReactDOM.createPortal(
+            <Backdrop onClose={clickHandle} />,
+            portalElement
+          )}
+          {ReactDOM.createPortal(
+            <ModalOverlay>{children}</ModalOverlay>,
+            portalElement
+          )}
+        </Fragment>
       )}
     </>,
     document.getElementById("portal")
