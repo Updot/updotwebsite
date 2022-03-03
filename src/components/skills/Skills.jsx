91f7cec22f4c1ca2d33f3069f20d4b9081e1defd
Sourab <shubpramanik241@gmail.com>
2022-03-03 15:17:21 +0530

    cleanup

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,24 +1,15 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import SectionHeading from "../ui/SectionHeading";
 import ScrollDot from "./ScrollDot";
 import SkillContainer from "./SkillContainer";
 
 import classes from "./Skills.module.scss";
 
-let count = 0;
 const Skills = () => {
   const [scrollDir, setScrollDir] = useState("bottom");
   const [scrollDotCount, setScrollDotCount] = useState(0);
-  const [parent, setParent] = useState(null);
 
-  const skillRef = useRef(null);
   const ghostRef = useRef(null);
-
-  useEffect(() => {
-    if (skillRef.current) {
-      setParent(skillRef.current);
-    }
-  }, []);
 
   const setScrollDot = (skillNum) => {
     setScrollDotCount(skillNum);
@@ -33,12 +24,8 @@ const Skills = () => {
       </div>
       <div ref={ghostRef} className={classes["ghost"]}>
         <div className={classes.skills}>
-          <div className={classes["skills-outer"]} ref={skillRef}>
-            <SkillContainer
-              scrollDir={scrollDir}
-              setScrollDot={setScrollDot}
-              parent={parent}
-            />
+          <div className={classes["skills-outer"]}>
+            <SkillContainer scrollDir={scrollDir} setScrollDot={setScrollDot} />
           </div>
         </div>
         {window.innerWidth < 800 && (
